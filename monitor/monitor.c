caab1b940c23209ea4ff49b0b9c120adb8ffdb89
Chris Fretz <Cfretz244@gmail.com>
2015-11-11 03:14:11 -0500

    added initial (untested) implementation of the task add functionality

--- monitor/monitor.c
+++ monitor/monitor.c
@@ -1,21 +1,31 @@
+/*----- System Includes -----*/
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
-#include "../include/uthash.h"
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <errno.h>
 #include <syslog.h>
 
+/*----- Local Includes -----*/
+
+#include "../include/uthash.h"
+
+/*----- Constant Declarations -----*/
+
 #define NOTGIOS_MONITOR_PORT 31089
 #define NOTGIOS_ACCEPT_TIMEOUT 60
 #define NOTGIOS_READ_TIMEOUT 20
 #define NOTGIOS_WRITE_TIMEOUT 4
 #define NOTGIOS_STATIC_BUFSIZE 256
+#define NOTGIOS_MAX_COMMANDS 16
+#define NOTGIOS_MAX_OPTIONS 4
+#define NOTGIOS_MAX_OPTION_LEN 16
 #define NOTGIOS_SUCCESS 0x0
 #define NOTGIOS_GENERIC_ERROR -0x01
 #define NOTGIOS_BAD_HOSTNAME -0x02
@@ -23,23 +33,67 @@
 #define NOTGIOS_SERVER_REJECTED -0x08
 #define NOTGIOS_SOCKET_FAILURE -0x10
 #define NOTGIOS_SOCKET_CLOSED -0x20
+#define NOTGIOS_TOO_MANY_ARGS -0x40
 
-int handshake(char *server_hostname, int port, int initial);
-void launch_worker_thread(void *args);
+/*----- Macro Declarations -----*/
+
+#define RETURN_NACK(buf, msg)                                       \
+  do {                                                              \
+    sprintf(buf, msg);                                              \
+    return;                                                         \
+  } while (0);
+
+/*----- Function Declarations -----*/
+
+void *launch_worker_thread(void *args);
+void handle_add(char **commands, char *reply_buf);
+void handle_pause(char **commands, char *reply_buf);
+void handle_resume(char **commands, char *reply_buf);
+void handle_delete(char **commands, char *reply_buf);
+
 int create_server();
+int handshake(char *server_hostname, int port, int initial);
 int handle_read(int fd, char *buffer, int len);
 int handle_write(int fd, char *buffer);
+int parse_commands(char **output, char *input);
 void user_error();
+
+/*----- Type Declarations -----*/
+
+typedef enum {
+  PROCESS,
+  DIRECTORY,
+  DISK,
+  SWAP,
+  LOAD
+} task_type_t;
+
+typedef enum {
+  MEMORY,
+  CPU,
+  IO,
+  NONE
+} metric_type_t;
+
+typedef char thread_option_t[NOTGIOS_MAX_OPTION_LEN];
+
+typedef struct thread_args {
+  int id, freq;
+  task_type_t type;
+  metric_type_t metric;
+  thread_option_t options[NOTGIOS_MAX_OPTIONS];
+} thread_args_t;
 
 /*----- Evil but Necessary Globals -----*/
 
-int connected;
+int connected = 0;
+
+/*----- Function Implementations -----*/
 
 int main(int argc, char **argv) {
   int c, port = 0, server_socket = 0, initial = 1;
   char *server_hostname = NULL;
 
-  connected = 0;
   opterr = 0;
   while ((c = getopt(argc, argv, "s:p:")) != -1) {
     switch (c) {
@@ -116,7 +170,43 @@ int main(int argc, char **argv) {
 
     // We're connected. Start reading jobs and stuff from the server.
     while (1) {
-      // Logic to read jobs from server.
+      int retval = handle_read(socket, buffer, NOTGIOS_STATIC_BUFSIZE);
+      if (retval > 0) {
+        char *commands[NOTGIOS_MAX_COMMANDS];
+        memset(buffer, 0, sizeof(char) * NOTGIOS_STATIC_BUFSIZE);
+
+        if (parse_commands(commands, buffer)) {
+          char *cmd = commands[0];
+          if (strstr(cmd, "NGS JOB ADD") == cmd) {
+            handle_add(commands, buffer);
+          } else if (strstr(cmd, "NGS JOB PAUS") == cmd) {
+            handle_pause(commands, buffer);
+          } else if (strstr(cmd, "NGS JOB RES") == cmd) {
+            handle_resume(commands, buffer);
+          } else if (strstr(cmd, "NGS JOB DEL") == cmd) {
+            handle_delete(commands, buffer);
+          } else if (strstr(cmd, "NGS STILL THERE?") == cmd) {
+            // Manual keepalive. I know TCP is supposed to do stuff like this on its own, but honestly it makes
+            // it easier on my end to detect errors if I also do it manually.
+            sprintf(buffer, "NGS STILL HERE!\n\n");
+          } else if (strstr(cmd, "NGS BYE")) {
+
+          } else {
+            // Shouldn't happen, but hey, everything that isn't supposed to happen eventually does, so there.
+            sprintf(buffer, "NGS NACK\nCAUSE UNRECOGNIZED_COMMAND\n\n");
+          }
+        } else {
+          // The command we were sent is over the max size limit.
+          // Could dynamically allocate memory, but, since we're taking input from the user for this, there should
+          // be some sane upper limit to the command size anyways, in case they manage to slip something clever in,
+          // so this will indicate that possibility.
+          sprintf(buffer, "NGS NACK\nCAUSE COMMAND_TOO_LONG\n\n");
+        }
+
+        handle_write(socket, buffer);
+      } else {
+        break;
+      }
     }
 
     // Either our socket has been unexpectedly closed (server crashed), or we received an orderly
@@ -131,6 +221,79 @@ int main(int argc, char **argv) {
   }
 }
 
+void handle_add(char **commands, char *reply_buf) {
+  int id, freq;
+  char *type_str, *metric_str;
+  task_type_t type;
+  metric_type_t metric;
+
+  // Pull out the parameters.
+  sscanf(commands[1], "ID %d", &id);
+  sscanf(commands[2], "TYPE %s", type_str);
+  sscanf(commands[3], "METRIC %s", metric_str);
+  sscanf(commands[4], "FREQ %d", &freq);
+
+  // "Convert" from string to enum value.
+  if (!strcmp(type_str, "PROCESS")) type = PROCESS;
+  else if (!strcmp(type_str, "DIRECTORY")) type = DIRECTORY;
+  else if (!strcmp(type_str, "DISK")) type = DISK;
+  else if (!strcmp(type_str, "SWAP")) type = SWAP;
+  else if (!strcmp(type_str, "LOAD")) type = LOAD;
+  else RETURN_NACK(reply_buf, "NGS NACK\nCAUSE UNRECOGNIZED_TYPE\n\n");
+
+  // "Convert" from string to enum value.
+  if (!strcmp(metric_str, "MEMORY")) metric = MEMORY;
+  else if (!strcmp(metric_str, "CPU")) metric = CPU;
+  else if (!strcmp(metric_str, "IO")) metric = IO;
+  else if (!strcmp(metric_str, "NONE")) metric = NONE;
+  else RETURN_NACK(reply_buf, "NGS NACK\nCAUSE UNRECOGNIZED_METRIC\n\n");
+
+  // Get information ready to pass onto the thread.
+  thread_args_t *arguments = calloc(1, sizeof(thread_args_t));
+  arguments->id = id;
+  arguments->freq = freq;
+  arguments->type = type;
+  arguments->metric = metric;
+
+  // Look over rest of commands if applicable.
+  if (type == PROCESS || type == DISK) {
+    int elem = 0;
+    char *error = "NGS NACK\nCAUSE INAPPLICABLE_OPTION\n\n";
+
+    // This is really too long, but I love that I can do all of this on one line, so I'm leaving it on one line.
+    for (char **cmd_ptr = commands + 5, *cmd = *cmd_ptr; *cmd_ptr && cmd_ptr - commands < NOTGIOS_MAX_COMMANDS; cmd_ptr++, cmd = *cmd_ptr) {
+      // The structure of these if statements makes me sad. Maybe I'll refactor them sometime.
+      if (strstr("KEEPALIVE", cmd) == cmd || strstr("PIDFILE", cmd) == cmd || strstr("RUNCMD", cmd) == cmd) {
+        if (type != PROCESS) {
+          free(arguments);
+          RETURN_NACK(reply_buf, error);
+        }
+      } else if (strstr("MNTPNT", cmd) == cmd) {
+        if (type != DISK) {
+          free(arguments);
+          RETURN_NACK(reply_buf, error);
+        }
+      } else {
+        free(arguments);
+        RETURN_NACK(reply_buf, "NGS NACK\nCAUSE UNRECOGNIZED_OPTION\n\n");
+      }
+
+      memcpy(arguments->options[elem++], cmd, strlen(cmd) + 1);
+    }
+  }
+
+  // Create a new thread to run the task!
+  pthread_t task;
+  pthread_create(&task, NULL, launch_worker_thread, arguments);
+
+  // TODO: Need to insert newly created thread into a hash or something here.
+}
+
+// Performs handshake with server. Two different types of handshakes are possible and denote
+// slightly different things.
+// The first type happens during initial startup, and the other if the server goes down for
+// any reason. The first type assumes the server will send over all tasks for this host, and
+// the second does not.
 int handshake(char *server_hostname, int port, int initial) {
   int sockfd, actual = 0, sleep_period = 1;
   struct sockaddr_in serv_addr;
@@ -189,6 +352,17 @@ int create_server() {
   if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) return NOTGIOS_SOCKET_FAILURE;
   listen(server_fd, 10);
   return server_fd;
+}
+
+int parse_commands(char **output, char *input) {
+  int elem = 0;
+  memset(output, 0, sizeof(char *) * NOTGIOS_MAX_COMMANDS);
+  char *current = strtok(input, "\n");
+  do {
+    if (elem == 16) return NOTGIOS_TOO_MANY_ARGS;
+    output[elem++] = current;
+  } while (current = strtok(NULL, "\n"));
+  return NOTGIOS_SUCCESS;
 }
 
 // Should be an extremely fault tolerant wrapper around read.
